e65d253c152498a698667b584299dabda7de320b
robsongdev <robsong369@gmail.com>
2022-06-22 18:13:36 -0300

    Header Component CSS

---
+++ src/components/header/HeaderSocials.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import {BsLinkedin} from 'react-icons/bs'
+import{FaGithub} from 'react-icons/fa'
+
+const HeaderSocials = () => {
+    return(
+        <div className="header__socials">
+            <a href="http://linkedin.com" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+            <a href="http://github.com" target="_blank" rel="noreferrer"><FaGithub/></a>
+            {/* <a href="http://linkedin.com" target="_blank"></a> */}
+        </div>
+    )
+}
+
+export default HeaderSocials;
