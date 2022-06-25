a1922901572c91faf5a4ced7f09ea6a5d707cff6
robsongdev <robsong369@gmail.com>
2022-06-24 22:45:15 -0300

    Create About Component

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -5,8 +5,8 @@ import{FaGithub} from 'react-icons/fa'
 const HeaderSocials = () => {
     return(
         <div className="header__socials">
-            <a href="http://linkedin.com" target="_blank" rel="noreferrer"><BsLinkedin/></a>
-            <a href="http://github.com" target="_blank" rel="noreferrer"><FaGithub/></a>
+            <a href="https://www.linkedin.com/in/robson-gabriel-rodrigues" target="_blank" rel="noreferrer"><BsLinkedin/></a>
+            <a href="http://github.com/robsongdev" target="_blank" rel="noreferrer"><FaGithub/></a>
             {/* <a href="http://linkedin.com" target="_blank"></a> */}
         </div>
     )
